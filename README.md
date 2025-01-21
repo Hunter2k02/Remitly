b7a71dab385ba33867495f71cbb9d8c5adcc85ac
Filip Sanecki <philippo.sanecki@gmail.com>
2025-01-21 17:29:46 +0100

    Add more content to git ignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# Remitly
-Task for remitly
--- SWIFT/src/main/java/API/SWIFT/repository/HeadquarterRepository.java
+++
@@ -1,9 +0,0 @@
-package API.SWIFT.repository;
-
-import API.SWIFT.model.Headquarter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeadquarterRepository extends JpaRepository<Headquarter, Long> {
-}
